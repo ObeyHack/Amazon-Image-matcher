bb5bf757ed3a0fe2e730f0b72520825a446e4024
YonatanGH <yonatan.kratter@mail.huji.ac.il>
2024-07-14 10:59:15 +0300

    classifier functions update

--- imager/testing_classifier.py
+++ imager/testing_classifier.py
@@ -7,6 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
 from tqdm import tqdm
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity as cosine_similarity_sklearn
 
 def mapper_file_to_embeddings(file_name):
     with h5py.File(f"images/{file_name}.hdf5", 'r') as f:
@@ -15,9 +16,11 @@ def mapper_file_to_embeddings(file_name):
         csv_embeddings = f["images"][:]
         return file_name, csv_embeddings
 
+
 def mapper_embeddings_to_scores(file_name, embeddings, input_emb):
     scores = cosine_similarity(input_emb, embeddings)
     return file_name, scores
+
 
 def reducer_scores_to_max(file_name, scores, top_k=5):
     max_scores = np.sort(scores)[-top_k:]
@@ -40,6 +43,71 @@ def thread_mapReduce(file_names, input_emb, top_k=5):
     futures, _ = concurrent.futures.wait(futures)
     results = [future.result() for future in futures]
     return results
+
+
+def get_embedding_links(return_dict=False, hdf5_folder_path="images", csv_folder_path=r'..\Datasets'):
+    # Initialize an empty list to store embeddings and image links
+    all_embeddings = []
+    image_links = []
+
+    def _list_datasets(file_path):
+        """List all dataset names in an HDF5 file."""
+        with h5py.File(file_path, 'r') as hdf:
+            return [name for name in hdf.keys()]
+
+    # Iterate through each file in the folder
+    for file_name in os.listdir(hdf5_folder_path):
+        # if the hdf5 file is dataset.hdf5, continue
+        if file_name.endswith('.hdf5'):
+            file_path = os.path.join(hdf5_folder_path, file_name)
+            csv_file_path = os.path.join(csv_folder_path, file_name.replace('.hdf5', '.csv'))
+
+            dataset_names = _list_datasets(file_path)
+
+            with h5py.File(file_path, 'r') as hdf:
+                # Read the corresponding CSV file to get image links
+                df = pd.read_csv(csv_file_path)
+                for dataset_name in dataset_names:
+                    embeddings = hdf[dataset_name][:]
+                    if embeddings.size > 0:
+                        # Detect the actual embedding size
+                        actual_embedding_size = embeddings.shape[-1]
+                        for i, embedding in enumerate(embeddings):
+                            all_embeddings.append(embedding)
+                            image_links.append(df['image'].iloc[i])
+                    else:
+                        all_embeddings.append(np.zeros(actual_embedding_size))  # Handle empty embeddings case
+                        image_links.append(None)  # Handle empty embeddings case
+
+    # Convert lists to NumPy arrays
+    all_embeddings = np.array(all_embeddings)
+    image_links = np.array(image_links)
+
+    # Ensure the embeddings array is 2-dimensional
+    all_embeddings = all_embeddings.reshape(all_embeddings.shape[0], -1)
+
+    if return_dict:
+        # create a dictionary with embeddings to image links
+        embedding_links_dict = {}
+        for i in range(len(all_embeddings)):
+            embedding_links_dict[tuple(all_embeddings[i])] = image_links[i]
+        return embedding_links_dict
+
+    return all_embeddings, image_links
+
+
+def get_cosine_similarities(query_embedding, all_embeddings, k=0):
+    # Calculate cosine similarities
+    similarities = cosine_similarity_sklearn(query_embedding.reshape(1, -1), all_embeddings).flatten()
+    if k > 0:
+        if k > len(similarities):
+            print(f"Warning: k is larger than the number of embeddings. Setting k to {len(similarities)}")
+            k = len(similarities)
+
+        # Get indices of top k closest embeddings
+        top_k_indices = np.argsort(-similarities)[:k]
+        return similarities, top_k_indices
+    return similarities
 
 
 def main():
