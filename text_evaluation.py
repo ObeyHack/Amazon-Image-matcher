3d0bad1fe7e7097d2d6adce2ce1f39d8bb71b5d1
sean <Sean.Cherny.Israel13@gmail.com>
2024-07-14 12:23:45 +0300

    text

--- text_evaluation.py
+++ text_evaluation.py
@@ -1,10 +1,13 @@
-from urllib.request import urlopen
+import glob
+import zipfile
+from urllib.request import urlopen, build_opener
+import pandas as pd
 from bs4 import BeautifulSoup
-
 import torch
+from tqdm.auto import tqdm
 from transformers import BertModel, BertTokenizer
 from sklearn.metrics.pairwise import cosine_similarity
-
+import os
 model = BertModel.from_pretrained('bert-base-uncased')
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
@@ -41,7 +44,10 @@ def jaccard_similarity(doc1, doc2):
 
 
 def description_scraper(url):
-    response = urlopen(url)
+    opener = build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+
+    response = opener.open(url)
     html = response.read()
 
     # Parse the HTML content using BeautifulSoup
@@ -53,10 +59,14 @@ def description_scraper(url):
     if description_div:
         # Extract the text within the <p><span> tags
         description_text = description_div.find('p').find('span').get_text()
-        print(description_text)
     else:
         print("Description not found")
-    return description_text
+        description_text = ""
+
+    # Find the title of the product
+    title = soup.select_one('#productTitle').text
+
+    return description_text + title
 
 
 def similarity_score(url1, url2):
@@ -66,7 +76,32 @@ def similarity_score(url1, url2):
     return similarity
 
 
+def similar_links(text, urls):
+    similarities = []
+    text_emb = text_embedding(text)
+    for link in tqdm(urls):
+        description = description_scraper(link)
+        similarity = cosine_similarity(text_emb, text_embedding(description))
+        similarities.append((link, similarity))
+    return similarities
+
+
+def get_urls():
+    with zipfile.ZipFile("Datasets/archive.zip", 'r') as zip_ref:
+        zip_ref.extractall("Datasets")
+
+    df = pd.read_csv("Datasets/Amazon-Products.csv")
+    urls = df['link']
+    return urls
+
+
 if __name__ == '__main__':
-    url1 = 'https://www.amazon.com/Under-Armour-Charged-Assert-X-Wide/dp/B08CFT75X3/ref=sr_1_2?_encoding=UTF8&content-id=amzn1.sym.56e14e61-447a-443b-9528-4b285fddeeac&crid=1QEZIUFPCL3YZ&dib=eyJ2IjoiMSJ9.ft2_UOW6_812lc9l1-QSVp262n9lnrp9JkYxbzch50YDBc3lzBNyzMAiBk-I0IdyUcrfaGVjLJRshNC2heUyGwkRM8s0DoTb4M6iESi81wnkVgmzqAjgcRlkbEfcDI24cTaNoVMc3Mdool0oekYx_66W7cs9xa5ygzH_QQjvrB0aNX-Mz-IKmLBuA6CGzSxzDgw_WbXkr6Xhdj7AwUuSIj9YhQVnyp4PvUZ3YtcB7qdUQcQHrIv325on_XbSy7GY5SU2aZGHOTLcpAiBLoyJGZCQLeNUz3abwIVYKtMoNGI.ThotIlFS47Lro8cttfDqEFWQr5sueLmTYX1UdYgp-yg&dib_tag=se&keywords=Shoes&pd_rd_r=20072d94-7d9c-4817-9c8a-c541f1ee3e84&pd_rd_w=iiWNK&pd_rd_wg=UgsLQ&pf_rd_p=56e14e61-447a-443b-9528-4b285fddeeac&pf_rd_r=C7WVXH61VXFF81AV2G2Y&qid=1720343515&refinements=p_36%3A-5000&rnid=2661611011&sprefix=shoes%2Caps%2C145&sr=8-2&th=1'
-    url2='https://www.amazon.com/dp/B0BZXMXTT2/ref=sspa_dk_detail_1?psc=1&pf_rd_p=386c274b-4bfe-4421-9052-a1a56db557ab&pf_rd_r=FND5XJ34Y17881CR2G9D&pd_rd_wg=Yvcge&pd_rd_w=cexMT&content-id=amzn1.sym.386c274b-4bfe-4421-9052-a1a56db557ab&pd_rd_r=c790c2c3-81de-4a2c-b1a0-9b79447aab15&s=shoes&sp_csd=d2lkZ2V0TmFtZT1zcF9kZXRhaWxfdGhlbWF0aWM'
-    print(similarity_score(url1, url2))
+    urls = get_urls()
+    text = "A stylish and comfortable pair of shoes for everyday wear"
+    similarities = similar_links(text, urls)
+
+
+
+    # url1 = 'https://www.amazon.com/Under-Armour-Charged-Assert-X-Wide/dp/B08CFT75X3/ref=sr_1_2?_encoding=UTF8&content-id=amzn1.sym.56e14e61-447a-443b-9528-4b285fddeeac&crid=1QEZIUFPCL3YZ&dib=eyJ2IjoiMSJ9.ft2_UOW6_812lc9l1-QSVp262n9lnrp9JkYxbzch50YDBc3lzBNyzMAiBk-I0IdyUcrfaGVjLJRshNC2heUyGwkRM8s0DoTb4M6iESi81wnkVgmzqAjgcRlkbEfcDI24cTaNoVMc3Mdool0oekYx_66W7cs9xa5ygzH_QQjvrB0aNX-Mz-IKmLBuA6CGzSxzDgw_WbXkr6Xhdj7AwUuSIj9YhQVnyp4PvUZ3YtcB7qdUQcQHrIv325on_XbSy7GY5SU2aZGHOTLcpAiBLoyJGZCQLeNUz3abwIVYKtMoNGI.ThotIlFS47Lro8cttfDqEFWQr5sueLmTYX1UdYgp-yg&dib_tag=se&keywords=Shoes&pd_rd_r=20072d94-7d9c-4817-9c8a-c541f1ee3e84&pd_rd_w=iiWNK&pd_rd_wg=UgsLQ&pf_rd_p=56e14e61-447a-443b-9528-4b285fddeeac&pf_rd_r=C7WVXH61VXFF81AV2G2Y&qid=1720343515&refinements=p_36%3A-5000&rnid=2661611011&sprefix=shoes%2Caps%2C145&sr=8-2&th=1'
+    # url2='https://www.amazon.com/dp/B0BZXMXTT2/ref=sspa_dk_detail_1?psc=1&pf_rd_p=386c274b-4bfe-4421-9052-a1a56db557ab&pf_rd_r=FND5XJ34Y17881CR2G9D&pd_rd_wg=Yvcge&pd_rd_w=cexMT&content-id=amzn1.sym.386c274b-4bfe-4421-9052-a1a56db557ab&pd_rd_r=c790c2c3-81de-4a2c-b1a0-9b79447aab15&s=shoes&sp_csd=d2lkZ2V0TmFtZT1zcF9kZXRhaWxfdGhlbWF0aWM'
+    # print(similarity_score(url1, url2))
