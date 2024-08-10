82134cec293121548d798d68c30a3f58da970640
michaelmerzin <120664244+michaelmerzin@users.noreply.github.com>
2024-08-10 17:56:20 +0300

    eval

--- model_evaluation/model.py
+++ model_evaluation/model.py
@@ -2,8 +2,8 @@ import os
 from imager.image_classifier import get_embedding_links, get_cosine_similarities
 import pandas as pd
 from imager.vgg.VGG import get_image_embeddings, load_image_bytes
-
-
+import text_evaluation
+import numpy as np
 class AmazonModel:
     def __init__(self, root_path=os.getcwd() + "/../",k=5):
         all_embeddings, image_links = get_embedding_links(hdf5_folder_path=root_path + "imager/images/")
@@ -27,15 +27,31 @@ class AmazonModel:
 
         return links
 
+    def eval_model(self,X, y):
+        # X is the test data, y is the labels of the test data, model is the model that we will evaluate
+        # data
+        accuracy = 0
+        for i in range(len(X)):
+            x = X[i]
+            y = y[i]
+            score = eval(x, y)
+            # if score > 0.5:
+            #    accuracy += 1
+            # or
+            accuracy += score
 
-    def eval(self, x, y):
+        return accuracy / len(X)
+
+    def eval(self,x, y):
         """
         :param x: an image
         :param y: text description
         :return: int - evaluation score
         """
-        y_hat = self.predict(x) # get the top k predictions
-        pass
+        top_k_predictions = self.predict(x)
+        top_k_predictions_text = [text_evaluation.description_scraper(url) for url in top_k_predictions]
+        similarity_scores = [text_evaluation.similarity_score(text, y) for text in top_k_predictions_text]
+        return np.mean(similarity_scores)
 
 
 
