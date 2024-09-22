e71201fe660a93c1e62f51fb3a32d8fbf4ba24e0
MrObay <Sean.Cherny.Israel13@gmail.com>
2024-09-22 16:21:18 +0300

    clusters

---
+++ imager/clusters.py
@@ -0,0 +1,12 @@
+import sklearn
+from sklearn.cluster import KMeans
+from imager.image_classifier import get_embedding_links
+
+def main():
+    print("Hello World")
+    embs = get_embedding_links()
+    x = 1
+
+
+if __name__ == '__main__':
+    main()
